9a4e28e83502eab021422d1543d29bba83eaafb3
Ryan Cleary <rcleary@usc.edu>
2012-12-18 15:35:47 -0800

    more work on url stuff

--- launchURL.php
+++ launchURL.php
@@ -3,13 +3,16 @@
 // Called from "landing.html" upon entering a URL
 
 $url = $_GET['URLfieldName'];
-if (urlParse($url)){
+if ($url != null && $url != "" && $url != " "){
 	// Do the URL Parsing Here.
 	echo("2 url parse");
-	//header( 'Location: http://'.$url);
+	header( 'Location: http://'.$url);
 } else {
 	header( 'Location: landing.html');
 }
+
+/** STILL WORKING ON THIS (Ryan) **/
+
 
 function urlParse($url)
 {
@@ -17,16 +20,48 @@ function urlParse($url)
 
 	$http = substr($url, 0,7);    // "http://"
 	$https = substr($url, 0,8);    // "https://"
+	$www = substr($url, 0,4); // www.
 
 
-	if (strcmp($http,"http://") == 0 || substr($url, 0,7))    // "http://"
+	if ((strcmp($http,"http://") == 0 || (strcmp($https,"https://") == 0 )))    // string either starts with 'http://' or 'https://'
 	{
-		
+		if (preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url)) // this line from http://phpcentral.com/208-url-validation-in-php.html
+		{
+			// The string is a valid URL
+			echo($url);
+			return $url;
+		}
+		else
+		{
+			$returnStr = bingURL($url);
+			echo("http attempt invalid; bing str = ".$returnStr);
+			return $returnStr;
+		}
 	}
-
-	if ($url)
-
-	return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url); // this line from http://phpcentral.com/208-url-validation-in-php.html
+	else if (strcmp($www,"www.") == 0) // string starts with 'www.'
+	{
+		$returnStr = "http://".$url; // append 'http://' to the beginning of the url string
+		echo($returnStr);
+		return $returnStr;
+	}
+	else // BING IT!
+	{
+		$returnStr = bingURL($url);
+		echo($returnStr);
+		return $returnStr;
+	}
 }
 
+function bingURL($str)
+{
+		$returnStr = "http://www.bing.com/?q=".str_replace(" ","+",$url); // Bing search url is more complex than this, but this is a good start
+		return $returnStr;
+}
+
+
+
+
 ?>
+
+
+
