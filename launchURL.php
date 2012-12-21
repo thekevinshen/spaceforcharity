b069f7fb300ab179fe79aa8dcf8415792b86c053
Ryan Cleary <rcleary@usc.edu>
2012-12-20 23:30:41 -0800

    Did considerable amount of work to parse the urls.

--- launchURL.php
+++ launchURL.php
@@ -1,12 +1,14 @@
-<?php
+$url = "https:/www.google.com/";
 
-// Called from "landing.html" upon entering a URL
-
-$url = $_GET['URLfieldName'];
 if ($url != null && $url != "" && $url != " "){
 	// Do the URL Parsing Here.
-	echo("2 url parse");
-	header( 'Location: http://'.$url);
+	
+	urlParse($url); // passed by reference
+	
+	echo("Final URL = \n\n\n\n");
+	echo($url);
+	
+	//header( 'Location: '.$url);
 } else {
 	header( 'Location: landing.html');
 }
@@ -14,7 +16,8 @@ if ($url != null && $url != "" && $url != " "){
 /** STILL WORKING ON THIS (Ryan) **/
 
 
-function urlParse($url)
+
+function urlParse(&$url)
 {
 	$url = trim($url); // removes whitespaces from beginning and end
 
@@ -22,46 +25,77 @@ function urlParse($url)
 	$https = substr($url, 0,8);    // "https://"
 	$www = substr($url, 0,4); // www.
 
+//	echo("http ===> ".$http."\n");
+//	echo("https ===> ".$https."\n");
+//	echo("www ===> ".$www."\n");
 
 	if ((strcmp($http,"http://") == 0 || (strcmp($https,"https://") == 0 )))    // string either starts with 'http://' or 'https://'
 	{
 		if (preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url)) // this line from http://phpcentral.com/208-url-validation-in-php.html
 		{
 			// The string is a valid URL
-			echo($url);
 			return $url;
 		}
 		else
 		{
-			$returnStr = bingURL($url);
-			echo("http attempt invalid; bing str = ".$returnStr);
-			return $returnStr;
+			bingURL($url);
 		}
 	}
 	else if (strcmp($www,"www.") == 0) // string starts with 'www.'
 	{
-		$returnStr = "http://".$url; // append 'http://' to the beginning of the url string
-		echo($returnStr);
-		return $returnStr;
+		$url = "http://".$url; // append 'http://' to the beginning of the url string
+	}
+	else if ((strpos($url," ") === false) && 
+	strContainsPeriodWithCharactersBeforeAndAfterIt($url) === true) // triple equals checks for the boolean value of "was it found or not?"
+	{
+		$url = "http://www.".$url; // url was just missing the http://www. 	
 	}
 	else // BING IT!
 	{
-		$returnStr = bingURL($url);
-		echo($returnStr);
-		return $returnStr;
+		bingURL($url); // Create the bing search string (passed by reference)
 	}
 }
 
-function bingURL($str)
+function bingURL(&$url)
 {
-		$returnStr = "http://www.bing.com/?q=".str_replace(" ","+",$url); // Bing search url is more complex than this, but this is a good start
-		return $returnStr;
+		$url = "http://www.bing.com/?q=".str_replace(" ","+",$url); // Bing search url is more complex than this, but this is a good start
+		return $url;
 }
 
 
+function containsStrFromArr($str, array $arr)
+{
+    foreach($arr as $a) {
+        if (stripos($str,$a) !== false) return true; // insensitive comparison, tries to find first instance of a in str
+    }
+    return false;
+}
+
+function strContainsPeriodWithCharactersBeforeAndAfterIt($str)
+{
+	if (stripos($str,".") !== false) // make sure the string contains a period first
+	{
+		$position = stripos($str,".");
+													// testing.com
+		$strAfterPeriod = substr($str,$position+1,1); // c
+		$strBeforePeriod = substr($str,$position-1,1); // g
+		
+		//echo("before.=".$strBeforePeriod."\n");
+		//echo("after.=".$strAfterPeriod."\n");
+		
+		if (strcmp($strAfterPeriod," ") != 0 &&
+			strcmp($strBeforePeriod," ") != 0 && strlen($str) != ($position+1))
+			{
+				return true;
+			}
+	}
+	
+	//echo("\n\n containssvalidperiod = false!");
+	return false;
+	
+}
 
 
-?>
 
 
 
