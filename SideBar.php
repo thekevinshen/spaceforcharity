5bdd595c764455ef37a6c41612e0a4a501581bd9
Kevin Shen <shenkevin@gmail.com>
2012-12-29 23:18:07 -0800

    update sidebar gotourl, about page, styling

--- SideBar.php
+++ SideBar.php
@@ -4,7 +4,10 @@
 	<!--jQuery-->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
-	<script src="custom.js"></script>
+	<script type="text/javascript" src="custom.js"></script>
+	
+	<!--google web fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Cabin:400,700|Lobster+Two:400,700' rel='stylesheet' type='text/css'>
 	
 	<?php
 	
